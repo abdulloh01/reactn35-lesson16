b844d86181b9f622416f76b25e25fb841cbd4bc2
abdulloh01 <abdulloh04062008@gmail.com>
2023-10-31 05:59:54 +0500

    first deploy

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+    {
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:false
+        }
+    }
+}
+  );
+});
+
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
